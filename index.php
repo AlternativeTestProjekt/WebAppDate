3b35f4386cab98ba3f4a1be15921b3780af828ec
lentz <lentz@PBC215.publicare-gmbh.local>
2017-02-16 13:59:56 +0100

    Bootstrap Design eingebunden

--- index.php
+++ index.php
@@ -4,14 +4,35 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="de">
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
     </head>
-    <body>
-        <?php
-        echo 'Test';
-        ?>
-    </body>
+<body>
+  <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Match</a>
+                <a class="nav-item nav-link" href="#">Pricing</a>
+                <a class="nav-item nav-link" href="#">Kontakt</a>
+                
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Benutzername">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Passwort">
+                    <button class="btn btn-outline-success my-2 my-sm-0 mr-sm-2" type="submit">Login</button>
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Registrieren</button>
+                </form>
+            </div>
+        </div>
+  </nav>
+    <div class="col-md-12"></div>
+</body>
 </html>
